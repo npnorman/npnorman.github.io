f4855437cc0039df48106f16bebc2cb7efba491f
npnorman <97370227+theblindkarp@users.noreply.github.com>
2024-09-07 20:08:39 -0400

    made tes game

--- js/test-game.js
+++ js/test-game.js
@@ -1,7 +1,34 @@
-function setup() {
-    createCanvas(400, 400);
+// it tycoon.js
+
+//important html elements
+bucks = document.getElementById("bucks");
+jobPanel = document.getElementById("job-panel");
+potatoPanel = document.getElementById("potato-panel");
+
+//amounts
+bucksAmt = 0;
+
+function updateDisplay() {
+    bucks.innerHTML = bucksAmt;
 }
 
-function draw() {
-    background(220, 0, 200);
+function addBucks(amt) {
+    bucksAmt += amt;
 }
+
+jobPanel.addEventListener("click", function(e) {
+    //check for button
+    if(e.target.tagName === "BUTTON") {
+        //call a function
+        addBucks(1);
+        updateDisplay();
+    }
+});
+
+potatoPanel.addEventListener("click", function(e) {
+    //check for button
+    if(e.target.tagName === "BUTTON") {
+        //call a function
+        
+    }
+});
