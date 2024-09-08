d292e7a86f7ce048b40c7e4cd9fac7ce2cdea6b7
npnorman <97370227+theblindkarp@users.noreply.github.com>
2024-09-07 20:54:31 -0400

    added watt button

--- js/test-game.js
+++ js/test-game.js
@@ -2,18 +2,35 @@
 
 //important html elements
 bucks = document.getElementById("bucks");
+watts = document.getElementById("watts");
 jobPanel = document.getElementById("job-panel");
 potatoPanel = document.getElementById("potato-panel");
 
 //amounts
 bucksAmt = 0;
+wattsAmt = 0;
 
 function updateDisplay() {
     bucks.innerHTML = bucksAmt;
+    watts.innerHTML = wattsAmt;
 }
 
 function addBucks(amt) {
     bucksAmt += amt;
+}
+
+function addWatts(amt) {
+    wattsAmt += amt;
+}
+
+function loop() {
+    
+    while(true) {
+        setTimeout(function() {
+            addWatts(1);
+            updateDisplay();
+        }, 5000);
+    }
 }
 
 jobPanel.addEventListener("click", function(e) {
@@ -29,6 +46,7 @@ potatoPanel.addEventListener("click", function(e) {
     //check for button
     if(e.target.tagName === "BUTTON") {
         //call a function
-        
+        e.target.remove();
+        loop();
     }
 });
