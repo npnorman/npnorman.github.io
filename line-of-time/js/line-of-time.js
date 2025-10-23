4c77a26a384b7a60bf1bb457e120f82a4a2c1057
npnorman <npn.not@gmail.com>
2025-10-22 23:30:03 -0400

    cards displaying

--- line-of-time/js/line-of-time.js
+++ line-of-time/js/line-of-time.js
@@ -12,14 +12,18 @@ class Card {
         this.color = "#787878ff";
     }
 
-    createCard(guessName=null, color=null) {
+    setGuesser(guessName) {
+        this.guessName = guessName;
+    }
+
+    createCard(guessName=null) {
         //create and return card element
         if (guessName != null) {
             this.guessName = guessName;
         }
 
-        if (color != null) {
-            this.color = color;
+        if (guessName in guesserList) {
+            this.color = guesserList[guessName];
         }
 
         //create elements
@@ -71,14 +75,100 @@ var guesserList = {
 var startBtn = document.getElementById("startButton");
 var currentGuessCardDiv = document.getElementById("currentCard");
 var timelineDiv = document.getElementById("timeline");
+var questionsTextArea = document.getElementById("questions");
+var errorP = document.getElementById("error");
+
+//helper functions
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 //functions
 function startGame() {
+    resetGame();
+
     //parse from list
+    parseQuestions();
     //set up lists
     //call first create card for guess
-    var card = new Card("Cobol Invented", "1956");
-    currentGuessCardDiv.appendChild(card.createCard("Nick","#b20000ff"));
+    //initialize first guess
+    var randomIndex = getRandomIntInclusive(0,unusedCards.length-1);
+    var randomCard = unusedCards.splice(randomIndex,1)[0];
+    timelineCards.push(randomCard);
+
+    createTimeline();
+}
+
+function resetGame() {
+    clearTimeline();
+    errorP.textContent = "";
+    unusedCards = [];
+    timelineCards = [];
+    currentGuessCard = null;
+}
+
+function parseQuestions() {
+    //parse cards
+    //create cards
+    //add to unused list
+    var questions = questionsTextArea.value;
+    var lines = questions.split('\n');
+    for (var i=0; i < lines.length; i++) {
+        var line = lines[i];
+        var cardInfo = line.split(',');
+
+        if (cardInfo.length < 2) {
+            errorP.textContent = "Not enough information on line " + (i + 1);
+        }
+
+        if (cardInfo.length == 2) {
+            var newCard = new Card(cardInfo[0],cardInfo[1]);
+        } else if (cardInfo.length == 3) {
+            var newCard = new Card(cardInfo[0],cardInfo[1],cardInfo[2]);
+        }
+
+        unusedCards.push(newCard);
+    }
+}
+
+function setCurrentCard(card) {
+    while (currentGuessCardDiv.firstChild) {
+        currentGuessCardDiv.removeChild(currentGuessCardDiv.lastChild);
+    }
+
+    currentGuessCard.appendChild(card.createCard())
+}
+
+function turn() {
+    //defines a "turn" of the game
+    var card = new Card("Cobol Invented", "1959");
+    timelineDiv.appendChild(card.createCard("Nick","#b20000ff"));
+}
+
+function createTimeline() {
+    clearTimeline();
+    var tempButton = document.createElement("button");
+    tempButton.textContent = "+";
+    tempButton.classList.add("addButton");
+    tempButton.value = "0"
+
+    timelineDiv.appendChild(tempButton);
+    for (var i=0; i < timelineCards.length; i++) {
+        var tempCard = timelineCards[i].createCard();
+        timelineDiv.appendChild(tempCard);
+
+        var newButton = tempButton.cloneNode(true);
+        newButton.value = i.toString();
+        timelineDiv.appendChild(tempButton.cloneNode(true));
+    }
+}
+
+function clearTimeline() {
+    while (timelineDiv.firstChild) {
+        timelineDiv.removeChild(timelineDiv.lastChild);
+    }
 }
 
 //connections
