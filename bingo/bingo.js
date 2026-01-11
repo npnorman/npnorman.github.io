79e3d940d0c9130ff0e9fd84532849ed7aec193a
npnorman <npn.not@gmail.com>
2026-01-11 14:19:27 -0500

    updated bingo look

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -5,6 +5,7 @@ var callBtn = document.getElementById("call");
 var currentNumP = document.getElementById("current-num");
 var calledNumsP = document.getElementById("called-nums");
 var setBtn = document.getElementById("set");
+var noticeP = document.getElementById("notice");
 
 var numbers = [];
 
@@ -23,8 +24,62 @@ function resetNumbers() {
 
     //reset called numbers
     calledNumsP.textContent = "";
+
     //reset current num
     currentNumP.textContent = "";
+
+    //enable call button
+    callBtn.disabled = false;
+
+    //notice disabled
+    noticeP.style.display = "none";
+
+    //background color
+    currentNumP.style.backgroundColor = "white";
+}
+
+function addToCalledList(num) {
+    calledNumsP.innerHTML += "<span class=\"mini-ball\">" + num + "</span> ";
+}
+
+function setRandomNumber(num, ms) {
+
+    if (ms <= 100) {
+        //set num
+        currentNumP.textContent = num;
+
+        //set text color to red
+        currentNumP.style.color = "red";
+        
+        //add to called
+        addToCalledList(num);
+    
+    } else {
+        let randIndex = getRandomInt(0, numbers.length - 1);
+        currentNumP.textContent = numbers[randIndex];
+        setTimeout(function() {
+            setRandomNumber(num, ms - 20);
+        }, ms - 10);
+    }
+}
+
+function setAsLastCalled(num) {
+    //set text color to light
+    currentNumP.style.color = "rgb(255, 119, 119)";
+
+    // go through set random numbers with slowdown
+    setRandomNumber(num, 200);
+}
+
+function setEndOfList() {
+    // disable button
+    callBtn.disabled = true;
+
+    // display end of list
+    noticeP.style.display = "block";
+
+    // color last called background
+    currentNumP.style.backgroundColor = "pink";
 }
 
 function callNumber() {
@@ -37,16 +92,13 @@ function callNumber() {
     var randIndex = getRandomInt(0, numbers.length-1);
     var randNum = numbers[randIndex];
 
-    //add it to called numbers
-    calledNumsP.textContent += randNum + ", ";
-
     //set it as the current num
-    currentNumP.textContent = randNum;
+    setAsLastCalled(randNum);
 
     numbers.splice(randIndex, 1);
 
     if(numbers.length == 0) {
-        alert("End List")
+        setEndOfList();
     }
 }
 
