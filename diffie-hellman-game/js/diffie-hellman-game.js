733de7f9ef2d821b7cdb5de693c1b2ad59030c17
npnorman <npn.not@gmail.com>
2026-02-24 17:21:13 -0500

    added dh game

---
+++ diffie-hellman-game/js/diffie-hellman-game.js
@@ -0,0 +1,19 @@
+//diffie-hellman-game.js
+//Nicholas Norman February 2026
+//
+
+var k1 = document.getElementById("s-key")
+var k2 = document.getElementById("s-key2")
+var P = document.getElementById("P")
+var out1 = document.getElementById("out1")
+var out2 = document.getElementById("out2")
+var c1 = document.getElementById("c1")
+var c2 = document.getElementById("c2")
+
+c1.addEventListener('click', function () {
+    out1.innerHTML = Math.pow(6, k1.value) % 13;
+});
+
+c2.addEventListener('click', function () {
+    out2.innerHTML = Math.pow(P.value, k2.value) % 13;
+});
