c3d23f26837ba0d00c8c5ec9504e103908ded184
npnorman <npn.not@gmail.com>
2026-02-24 21:42:09 -0500

    added more to instructions

--- diffie-hellman-game/js/diffie-hellman-game.js
+++ diffie-hellman-game/js/diffie-hellman-game.js
@@ -3,17 +3,32 @@
 //
 
 var k1 = document.getElementById("s-key")
-var k2 = document.getElementById("s-key2")
 var P = document.getElementById("P")
 var out1 = document.getElementById("out1")
 var out2 = document.getElementById("out2")
 var c1 = document.getElementById("c1")
 var c2 = document.getElementById("c2")
+var checkbox = document.getElementById("cb")
 
 c1.addEventListener('click', function () {
     out1.innerHTML = Math.pow(5, k1.value) % 23;
 });
 
 c2.addEventListener('click', function () {
-    out2.innerHTML = Math.pow(P.value, k2.value) % 23;
+    out2.innerHTML = Math.pow(P.value, k1.value) % 23;
+});
+
+checkbox.addEventListener('click', function () {
+
+    let sc = document.getElementsByClassName("scared");
+
+    if (checkbox.checked) {
+        for (let i = 0; i < sc.length; i++) {
+            sc[i].classList.add("scared-on");
+        }
+    } else {
+        for (let i = 0; i < sc.length; i++) {
+            sc[i].classList.remove("scared-on");
+        }
+    }
 });
