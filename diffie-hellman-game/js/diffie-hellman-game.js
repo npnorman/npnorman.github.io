93daae4cfd105ff2d3845c2c73c516ca06e31e71
npnorman <npn.not@gmail.com>
2026-02-26 13:05:29 -0500

    Added sine styling to the diffie hellman game

--- diffie-hellman-game/js/diffie-hellman-game.js
+++ diffie-hellman-game/js/diffie-hellman-game.js
@@ -2,20 +2,33 @@
 //Nicholas Norman February 2026
 //
 
-var k1 = document.getElementById("s-key")
-var P = document.getElementById("P")
-var out1 = document.getElementById("out1")
-var out2 = document.getElementById("out2")
-var c1 = document.getElementById("c1")
-var c2 = document.getElementById("c2")
-var checkbox = document.getElementById("cb")
+var k1 = document.getElementById("s-key");
+var P = document.getElementById("P");
+var out1 = document.getElementById("out1");
+var out2 = document.getElementById("out2");
+var c1 = document.getElementById("c1");
+var c2 = document.getElementById("c2");
+var checkbox = document.getElementById("cb");
+
+var kout = document.getElementById("k-out");
+var pout = document.getElementById("p-out");
+var sout = document.getElementById("s-out");
 
 c1.addEventListener('click', function () {
-    out1.innerHTML = Math.pow(5, k1.value) % 23;
+    let pVal = Math.pow(5, k1.value) % 23;
+    out1.innerHTML = pVal;
+    out1.innerHTML = "<b>" + out1.innerHTML + "</b>";
+
+    kout.innerHTML = k1.value;
+    pout.innerHTML = pVal;
 });
 
 c2.addEventListener('click', function () {
-    out2.innerHTML = Math.pow(P.value, k1.value) % 23;
+    let sVal = Math.pow(P.value, k1.value) % 23;
+    out2.innerHTML = sVal;
+    out2.innerHTML = "<b>" + out2.innerHTML + "</b>";
+
+    sout.innerHTML = sVal;
 });
 
 checkbox.addEventListener('click', function () {
