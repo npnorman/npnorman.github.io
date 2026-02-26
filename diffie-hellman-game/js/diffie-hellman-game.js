dfb216dfcd83fa7df340ac56a931b94f62f52af2
npnorman <npn.not@gmail.com>
2026-02-26 15:31:01 -0500

    Added cipher and math in depth

--- diffie-hellman-game/js/diffie-hellman-game.js
+++ diffie-hellman-game/js/diffie-hellman-game.js
@@ -2,6 +2,10 @@
 //Nicholas Norman February 2026
 //
 
+function mod(n, d) {
+  return ((n % d) + d) % d;
+}
+
 var k1 = document.getElementById("s-key");
 var P = document.getElementById("P");
 var out1 = document.getElementById("out1");
@@ -13,6 +17,14 @@ var checkbox = document.getElementById("cb");
 var kout = document.getElementById("k-out");
 var pout = document.getElementById("p-out");
 var sout = document.getElementById("s-out");
+var sout2 = document.getElementById("s-out2");
+
+var encryptBtn = document.getElementById("encrypt");
+var decryptBtn = document.getElementById("decrypt");
+var plaintextBox = document.getElementById("plaintext");
+var ciphertextBox = document.getElementById("ciphertext");
+
+var s = 0;
 
 c1.addEventListener('click', function () {
     let pVal = Math.pow(5, k1.value) % 23;
@@ -29,6 +41,8 @@ c2.addEventListener('click', function () {
     out2.innerHTML = "<b>" + out2.innerHTML + "</b>";
 
     sout.innerHTML = sVal;
+    sout2.innerHTML = sVal;
+    s = sVal;
 });
 
 checkbox.addEventListener('click', function () {
@@ -45,3 +59,86 @@ checkbox.addEventListener('click', function () {
         }
     }
 });
+
+function shiftEncrypt(key, plaintext) {
+    if (key < 0 || key > 25) {
+        key = 0;
+    }
+
+    let ciphertext = "";
+
+    // for each letter in plaintext
+    for (let i = 0; i < plaintext.length; i++) {
+
+        if (plaintext[i].toLowerCase() == plaintext[i].toUpperCase()) {
+            // is not a letter
+            ciphertext += plaintext[i];
+
+        } else {
+            // convert to number
+            let charNum = plaintext[i].toLowerCase().charCodeAt(0) - 97;
+
+            // add shift
+            charNum += key;
+
+            // modulo to circle back
+            charNum = mod(charNum, 26);
+
+            ciphertext += String.fromCharCode(97 + charNum);
+        }
+    }
+    
+    // return ciphertext
+    return ciphertext.toUpperCase();
+}
+
+function shiftDecrypt(key, ciphertext) {
+    if (key < 0 || key > 25) {
+        key = 0;
+    }
+
+    let plaintext = "";
+
+    // for each letter in ciphertext
+    for (let i = 0; i < ciphertext.length; i++) {
+
+        if (ciphertext[i].toLowerCase() == ciphertext[i].toUpperCase()) {
+            // is not a letter
+            plaintext += ciphertext[i];
+
+        } else {
+            // convert to number
+            let charNum = ciphertext[i].toLowerCase().charCodeAt(0) - 97;
+
+            // subtract shift
+            charNum -= key;
+
+            // modulo to circle back
+            charNum = mod(charNum, 26);
+
+            plaintext += String.fromCharCode(97 + charNum);
+        }
+    }
+    
+    // return ciphertext
+    return plaintext;
+}
+
+encryptBtn.addEventListener('click', function () {
+    let plaintext = plaintextBox.value;
+    let ciphertext = shiftEncrypt(s,plaintext);
+
+    ciphertextBox.value = ciphertext;
+});
+
+decryptBtn.addEventListener('click', function () {
+    let ciphertext = ciphertextBox.value;
+    let plaintext = shiftDecrypt(s,ciphertext);
+
+    plaintextBox.value = plaintext;
+});
+
+let key = 25;
+let t = shiftEncrypt(key, "HELLO, ABCD");
+console.log(t);
+console.log(shiftDecrypt(key,t));
