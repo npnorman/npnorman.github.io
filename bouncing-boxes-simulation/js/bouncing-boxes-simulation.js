43d70a1550e9ef97fa25dba7dd14a1e68c14112f
npnorman <npn.not@gmail.com>
2026-02-27 23:01:43 -0500

    set up basic squares template

---
+++ bouncing-boxes-simulation/js/bouncing-boxes-simulation.js
@@ -0,0 +1,11 @@
+//bouncing-boxes-simulation.js
+//Nicholas Norman February 2026
+//Provide a simulation of boxes bouncing and interacting
+
+var squares = []
+
+function main() {
+    // update positions
+
+    // draw to screen
+}
